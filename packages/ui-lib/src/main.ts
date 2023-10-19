55713906f1b18500a4b8454015d2ea8961a98226
william chang <coooo77@hotmail.comcls>
2023-10-19 22:24:36 +0800

    create vue.js component library

--- packages/ui-lib/src/main.ts
+++ packages/ui-lib/src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import './style.css';
 
-createApp(App).mount('#app')
+import { App } from 'vue';
+
+// export individual components
+import MyBrandButton from './MyBrandButton/MyBrandButton.vue';
+
+export { MyBrandButton };
+
+// export the entire library as a plugin, so it can be installed via app.use()
+// comment this out if you don't want to install the entire library
+
+export default {
+  install: (app: App) => {
+    app.component('MyBrandButton', MyBrandButton);
+  },
+};
--- packages/ui-lib/vite.config.ts
+++ packages/ui-lib/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  build: {
+    emptyOutDir: false,
+    lib: {
+      entry: path.resolve(__dirname, 'src/main.ts'),
+      formats: ['es'],
+      name: 'UiLib',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          Vue: 'vue',
+        },
+      },
+    },
+  },
+  plugins: [vue(), dts()],
+});
